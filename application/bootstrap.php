4ee20ed4760a4e2121a8f01eadcf4f84dd88e760
KurochkinAlex <kurochkin.alex1995@yandex.ru>
2015-08-04 23:12:24 +0400

    Added bootstrap.php and commited common corrections

---
+++ application/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+require_once "core/route.php";
+require_once "core/controller.php";
+require_once "core/view.php";
+require_once "core/model.php";
+Route::getStart();
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+require_once "application/bootstrap.php";
+ini_set("display_errors", 1);
