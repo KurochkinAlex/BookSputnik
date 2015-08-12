9f92f9b895ad2eedcfd4e256b60614e71a89c7c3
Ivan <dozzmen@yandex.ru>
2015-08-12 17:08:45 +0400

    90% ready
    Will add dropdown menus

--- _optimized_html/js/common.js
+++ _optimized_html/js/common.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 
-
-
 	//Цели для Яндекс.Метрики и Google Analytics
 	$(".count_element").on("click", (function() {
 		ga("send", "event", "goal", "goal");
@@ -46,3 +44,34 @@ $(document).ready(function() {
 	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
 
 });
+
+$(document).ready(function() {
+
+    var owl = $("#owl-demo");
+
+    owl.owlCarousel({
+        items : 10, //10 items above 1000px browser width
+        itemsDesktop : [1000,5], //5 items between 1000px and 901px
+        itemsDesktopSmall : [900,3], // betweem 900px and 601px
+        itemsTablet: [600,2], //2 items between 600 and 0
+        itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
+        autoHeight : true,
+        lazyLoad : true
+    });
+
+    // Custom Navigation Events
+    $(".next").click(function(){
+        owl.trigger('owl.next');
+    })
+    $(".prev").click(function(){
+        owl.trigger('owl.prev');
+    })
+    $(".play").click(function(){
+        owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
+    })
+    $(".stop").click(function(){
+        owl.trigger('owl.stop');
+    })
+
+});
+
