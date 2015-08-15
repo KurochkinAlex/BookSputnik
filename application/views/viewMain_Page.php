bbe9ce4a5b0cbb466ac0c4ed1d68ef74a499a5e7
Ivan <dozzmen@yandex.ru>
2015-08-15 18:25:45 +0400

    Structurized with MVC. img/ renamed to images/ and fixed links to img/

---
+++ application/views/viewMain_Page.php
@@ -0,0 +1,225 @@
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <a href="#" class="slide_img" style="background-image:url(images/1.png)"></a>
+            </div>
+            <div class="item">
+                <a href="#" class="slide_img" style="background-image:url(images/2.png)"></a>
+            </div>
+        </div>
+        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div><!-- /.carousel -->
+
+    <div class="main">
+        <div class="container">
+            <div class="landing">
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <a href="#">
+                        <img src="images/new_books.jpg" alt=""/>
+                    </a>
+                </div>
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <a href="#">
+                        <img src="images/new_books.jpg" alt=""/>
+                    </a>
+                </div>
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <a href="#">
+                        <img src="images/new_books.jpg" alt=""/>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+                <div class="best_books">
+                    <hr class="best_books_divider" style="border: 0;border-bottom: 1px dashed #ccc;background: #999;width: 92%;margin-top: 40px;"/>
+                    <a href="#">
+                        <h4 class="title">MOST POPULAR BOOKS</h4>
+                    </a>
+                    <div class="best_books_slider">
+                        <div id="owl-demo" class="owl-carousel owl-theme">
+                            <div class="item">
+                                <a class="item_outside" href="#"><img src="images/book_1.jpg" alt=""/>
+                                    <span>View Item</span>
+                                </a>
+                            </div>
+                            <div class="item">
+                                <a href="#"><img src="images/book_2.jpg" alt=""/>
+                                    <div class="preview_book">
+                                        <span>Заголовок работы</span>
+                                        <a href="#" class="popup_content">Посмотреть</a>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="item">
+                                <a href="#"><img src="images/book_3.jpg" alt=""/>
+                                    <div class="preview_book">
+                                        <span>Заголовок работы</span>
+                                        <a href="#" class="popup_content">Посмотреть</a>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="item">
+                                <a href="#"><img src="images/book_4.jpg" alt=""/>
+                                    <div class="preview_book">
+                                        <span>Заголовок работы</span>
+                                        <a href="#" class="popup_content">Посмотреть</a>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="item">
+                                <a href="#"><img src="images/book_5.jpg" alt=""/>
+                                    <div class="preview_book">
+                                        <span>Заголовок работы</span>
+                                        <a href="#" class="popup_content">Посмотреть</a>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="item">
+                                <a href="#"><img src="images/book_6.jpg" alt=""/>
+                                    <div class="preview_book">
+                                        <span>Заголовок работы</span>
+                                        <a href="#" class="popup_content">Посмотреть</a>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="item">
+                                <a href="#"><img src="images/book_7.jpg" alt=""/>
+                                    <div class="preview_book">
+                                        <span>Заголовок работы</span>
+                                        <a href="#" class="popup_content">Посмотреть</a>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="item">
+                                <a href="#"><img src="images/book_8.jpg" alt=""/>
+                                    <div class="preview_book">
+                                        <span>Заголовок работы</span>
+                                        <a href="#" class="popup_content">Посмотреть</a>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="item">
+                                <a href="#"><img src="images/book_9.jpg" alt=""/>
+                                    <div class="preview_book">
+                                        <span>Заголовок работы</span>
+                                        <a href="#" class="popup_content">Посмотреть</a>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="item">
+                                <a href="#"><img src="images/book_10.jpg" alt=""/>
+                                    <div class="preview_book">
+                                        <span>Заголовок работы</span>
+                                        <a href="#" class="popup_content">Посмотреть</a>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="item">
+                                <a href="#"><img src="images/book_11.jpg" alt=""/>
+                                    <div class="preview_book">
+                                        <span>Заголовок работы</span>
+                                        <a href="#" class="popup_content">Посмотреть</a>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="item">
+                                <a href="#"><img src="images/book_12.jpg" alt=""/>
+                                    <div class="preview_book">
+                                        <span>Заголовок работы</span>
+                                        <a href="#" class="popup_content">Посмотреть</a>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <div class="ship_info">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <a href="">
+                            <div class="ship_item">
+                                <svg class="ship_icon ship_car" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                 width="100px" height="100px" viewBox="0 0 612 612" style="enable-background:new 0 0 612 612;" xml:space="preserve">
+                                    <g>
+                                <g>
+                                    <path d="M226.764,375.35c-28.249,0-51.078,22.91-51.078,51.16c0,28.166,22.829,51.078,51.078,51.078s51.078-22.912,51.078-51.078
+                                                C277.841,398.26,255.013,375.35,226.764,375.35z M226.764,452.049c-14.125,0-25.54-11.498-25.54-25.541
+                                                c0-14.123,11.415-25.539,25.54-25.539c14.124,0,25.539,11.416,25.539,25.539C252.302,440.551,240.888,452.049,226.764,452.049z
+                                                 M612,337.561v54.541c0,13.605-11.029,24.635-24.636,24.635h-26.36c-4.763-32.684-32.929-57.812-66.927-57.812
+                                                c-33.914,0-62.082,25.129-66.845,57.812H293.625c-4.763-32.684-32.93-57.812-66.845-57.812c-33.915,0-62.082,25.129-66.844,57.812
+                                                h-33.012c-13.606,0-24.635-11.029-24.635-24.635v-54.541H612L612,337.561z M494.143,375.35c-28.249,0-51.16,22.91-51.16,51.16
+                                                c0,28.166,22.912,51.078,51.16,51.078c28.166,0,51.077-22.912,51.077-51.078C545.22,398.26,522.309,375.35,494.143,375.35z
+                                                 M494.143,452.049c-14.125,0-25.539-11.498-25.539-25.541c0-14.123,11.414-25.539,25.539-25.539
+                                                c14.042,0,25.539,11.416,25.539,25.539C519.682,440.551,508.185,452.049,494.143,452.049z M602.293,282.637l-96.817-95.751
+                                                c-6.159-6.077-14.453-9.526-23.076-9.526h-48.86v-18.313c0-13.631-11.004-24.635-24.635-24.635H126.907
+                                                c-13.55,0-24.635,11.005-24.635,24.635v3.86L2.3,174.429l177.146,23.068L0,215.323l178.814,25.423L0,256.25l102.278,19.29
+                                                l-0.007,48.403h509.712v-17.985C611.983,297.171,608.452,288.796,602.293,282.637z M560.084,285.839h-93.697
+                                                c-2.135,0-3.86-1.724-3.86-3.859v-72.347c0-2.135,1.725-3.86,3.86-3.86h17.82c0.985,0,1.971,0.411,2.71,1.068l75.796,72.347
+                                                C565.257,281.569,563.532,285.839,560.084,285.839z"/>
+                                </g>
+                            </g>
+                                </svg>
+                                <span>FREE SHIPPING <br/>ON ORDERS 75$ +</span>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="">
+                            <div class="ship_item">
+                                <svg class="ship_icon ship_box" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                             width="612px" height="612px" viewBox="0 0 612 612" style="enable-background:new 0 0 612 612;" xml:space="preserve">
+                                        <g>
+                                            <path d="M305.016,224.064v252.192l200.778-38.103V315.277c0-5.144,4.48-9.139,9.59-8.553l24.701,3.822
+                                                c4.346,0.499,7.627,4.178,7.627,8.553v142.748c0,4.058-2.833,7.564-6.8,8.417l-219.787,47.258
+                                                c-10.621,2.283-21.604,2.308-32.235,0.072L71.462,471.859c-3.902-0.82-6.73-4.214-6.834-8.201l-3.756-144.974
+                                                c-0.118-4.547,3.322-8.4,7.852-8.798l184.692-16.229c3.203-0.281,5.982-2.325,7.207-5.297l27.824-67.573
+                                                C292.037,212.067,305.016,214.635,305.016,224.064z M58.783,141.494L0.825,265.38c-2.832,6.054,2.014,12.885,8.663,12.213
+                                                L227.149,255.6c3.076-0.311,5.749-2.247,7.003-5.073l70.035-157.78L65.026,136.675C62.289,137.178,59.963,138.973,58.783,141.494z
+                                                 M304.188,92.747l74.106,162.712c1.295,2.844,4.029,4.762,7.145,5.013l217.25,17.471c6.547,0.526,11.248-6.178,8.522-12.154
+                                                l-56.658-124.212c-1.172-2.569-3.528-4.399-6.308-4.9L304.188,92.747z"/>
+                                        </g>
+                                </svg>
+
+                                <span>FREE RETURNS <br/>IN RUSSIA</span>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="">
+                            <div class="ship_item">
+                                <svg class="ship_icon ship_stopwatch" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                             width="612px" height="612px" viewBox="0 0 612 612" style="enable-background:new 0 0 612 612;" xml:space="preserve">
+                                        <g>
+                                            <path d="M543.981,160.777l-18.297,18.571c44.158,52.077,68.485,121.464,61.039,196.445
+                                                c-12.315,124.002-112.79,223.666-236.88,235.068c-146.688,13.479-271.77-94.371-286.28-234.55H37.712
+                                                c-15.717,0-25.54-17.015-17.681-30.626L77.85,245.565c7.859-13.609,27.503-13.608,35.361,0.001l57.806,100.122
+                                                c7.859,13.61-1.964,30.624-17.681,30.624h-22.505c14.622,104.658,112.057,183.152,223.772,166.778
+                                                c82.668-12.116,149.722-77.337,163.773-159.697c21.031-123.271-73.828-230.558-193.271-230.558
+                                                c-38.734,0-76.119,11.237-108.234,32.52c-14.904,9.876-35.564,7.922-46.498-6.224c-11.906-15.401-8.188-37.389,7.687-48.109
+                                                c34.455-23.276,73.515-37.822,114.438-42.886V50.184h-26.784c-9.216,0-16.686-7.47-16.686-16.686V16.686
+                                                C249.028,7.471,256.5,0,265.714,0h124.21c9.216,0,16.686,7.471,16.686,16.686v16.812c0,9.215-7.47,16.686-16.686,16.686h-29.501
+                                                v38.348c52.534,7.088,100.161,29.78,138.142,63.186l16.975-17.229l-12.439-11.499c-6.767-6.254-7.183-16.812-0.927-23.579
+                                                l13.31-14.4c6.255-6.768,16.812-7.183,23.579-0.928l50.312,46.505c6.769,6.255,7.183,16.812,0.929,23.579l-13.31,14.399
+                                                c-6.255,6.767-16.812,7.183-23.579,0.928L543.981,160.777z M361.604,314.24c-23.084-23.57-62.787-21.009-82.25,8.07
+                                                c-11.684,17.455-11.505,41.047,0.298,58.42c14.344,21.111,39.598,27.998,61.139,20.692l67.067,54.629
+                                                c12.822,10.443,29.816-6.398,19.489-19.313L373.5,369.393C380.224,350.844,376.333,329.28,361.604,314.24z"/>
+                                        </g>
+                                </svg>
+                                <span>90 - DAY <br/>RETURNS</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
